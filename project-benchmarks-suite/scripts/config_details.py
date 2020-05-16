ff02585cb4b679601178d8afe99504cbd45ffc6b
German Diago Gomez <germandiago1983@gmail.com>
2020-05-16 18:58:53 +0200

    Configuration details script updated

---
+++ project-benchmarks-suite/scripts/config_details.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import platform
+import os.path as op
+from Pathlib import Path
+
+from util import BUILD_DIR_BASE
+
+config_folder = '!'.join(CONFIG_NAMES)
+config_details_dir = 'config_details'
+
+for config in CONFIG_NAMES:
+    print(config_name)
+    #with open(Path(config_details_dir) / )
